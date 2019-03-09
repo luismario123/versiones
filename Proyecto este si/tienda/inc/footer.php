ffe05f56c86a3bfc23420e21e4656b7201bcb0db
kirito123321 <44415685+luismario123@users.noreply.github.com>
2019-03-08 20:50:21 -0600

    Add files via upload

---
+++ Proyecto este si/tienda/inc/footer.php
@@ -0,0 +1,43 @@
+<div class="container">
+      <!-- Example row of columns -->
+      <div class="row">
+        <div class="col-md-4">
+          <h2>Intermex</h2>
+          <p><img src="imagenes/Intermex.jpg" width="200" height="150"> </p>
+          <p><a class="btn btn-default" href="#" role="button">ir a Intermex &raquo;</a></p>
+        </div>
+        <div class="col-md-4">
+          <h2>Paga tus servicios</h2>
+          <p><img src="imagenes/pagar.jpg" width="200" height="150"></p>
+          <p><a class="btn btn-default" href="#" role="button">ir a servicios &raquo;</a></p>
+       </div>
+        <div class="col-md-4">
+          <h2>Invitaciones</h2>
+          <p><img src="imagenes/invitacion.jpg" width="200" height="150"></p>
+          <p><a class="btn btn-default" href="#" role="button">ir a invitaciones &raquo;</a></p>
+        </div>
+      </div>
+
+      <hr>
+
+      <footer>
+        <p>&copy; Emiliano's Company 2015</p>
+      </footer>
+    </div> <!-- /container -->        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.2.min.js"><\/script>')</script>
+
+        <script src="js/vendor/bootstrap.min.js"></script>
+
+        <script src="js/main.js"></script>
+
+        <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+        <script>
+            (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+            function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+            e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+            e.src='//www.google-analytics.com/analytics.js';
+            r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+            ga('create','UA-XXXXX-X','auto');ga('send','pageview');
+        </script>
+    </body>
+</html>
